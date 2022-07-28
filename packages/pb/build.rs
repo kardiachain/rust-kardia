4e3f2357e4d192d4e47dd2f290bcde38de5fea75
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-07-28 07:15:30 +0000

    feat: auto get proto files

--- packages/pb/build.rs
+++ packages/pb/build.rs
@@ -1,66 +1,21 @@
-extern crate prost_build;
+use prost_build::compile_protos;
+use walkdir::WalkDir;
 
 fn main() {
-    prost_build::compile_protos(
-        &[
-            "src/gogoproto/gogo.proto",
-            "src/kardiachain/blockchain/types.proto",
-            "src/kardiachain/consensus/types.proto",
-            "src/kardiachain/consensus/wal.proto",
-            "src/kardiachain/crypto/keys.proto",
-            "src/kardiachain/crypto/proof.proto",
-            "src/kardiachain/evidence/types.proto",
-            "src/kardiachain/p2p/conn.proto",
-            "src/kardiachain/p2p/pex.proto",
-            "src/kardiachain/p2p/types.proto",
-            "src/kardiachain/state/types.proto",
-            "src/kardiachain/txpool/types.proto",
-            "src/kardiachain/types/block.proto",
-            "src/kardiachain/types/canonical.proto",
-            "src/kardiachain/types/events.proto",
-            "src/kardiachain/types/evidence.proto",
-            "src/kardiachain/types/params.proto",
-            "src/kardiachain/types/types.proto",
-            "src/kardiachain/types/validator.proto",
-        ],
-        &["src", "/usr/local/include/"],
-    )
-    .unwrap();
+    let mut pb_filepaths: Vec<String> = Vec::new();
+
+    for entry in WalkDir::new("./src")
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| !e.file_type().is_dir())
+    {
+        let path = entry.path();
+        if let Some(ext) = path.extension() {
+            if ext == "proto" {
+                pb_filepaths.push(String::from(path.to_str().unwrap()));
+            }
+        }
+    }
+
+    compile_protos(&pb_filepaths, &["src"]).unwrap();
 }
-
-// fn config() -> prost_build::Config {
-//     let mut config = prost_build::Config::new();
-//     config.bytes(&["."]);
-//     config
-// }
-
-// fn make_protos(protos: &[&str]) {
-//     tonic_build::configure()
-//         .compile_with_config(config(), protos, &["."])
-//         .unwrap();
-// }
-
-// fn main() {
-//     let protos = vec![
-//         "kardiachain/blockchain/types.proto",
-//         "kardiachain/consensus/types.proto",
-//         "kardiachain/consensus/wal.proto",
-//         "kardiachain/crypto/keys.proto",
-//         "kardiachain/crypto/proof.proto",
-//         "kardiachain/evidence/types.proto",
-//         "kardiachain/libs/bits/types.proto",
-//         "kardiachain/p2p/conn.proto",
-//         "kardiachain/p2p/pex.proto",
-//         "kardiachain/p2p/types.proto",
-//         "kardiachain/state/types.proto",
-//         "kardiachain/txpool/types.proto",
-//         "kardiachain/types/block.proto",
-//         "kardiachain/types/canonical.proto",
-//         "kardiachain/types/events.proto",
-//         "kardiachain/types/evidence.proto",
-//         "kardiachain/types/params.proto",
-//         "kardiachain/types/types.proto",
-//         "kardiachain/types/validator.proto",
-//     ];
-//     make_protos(&protos);
-// }
