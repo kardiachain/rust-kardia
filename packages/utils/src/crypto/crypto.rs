76e3d028382552217afe36c890b7ac7729471198
PNKDuy <phngkhuongduy@gmail.com>
2022-08-22 15:03:25 +0700

    implementing generate_key() function and adjust kai_utils path in toml

--- packages/utils/src/crypto/crypto.rs
+++ packages/utils/src/crypto/crypto.rs
@@ -1,19 +1,14 @@
 // use p256::{self, PublicKey, SecretKey, ecdsa::{self, Error}};
-use secp256k1::{SecretKey, PublicKey, Secp256k1};
+use secp256k1::{rand::rngs::OsRng, SecretKey, Secp256k1};
 use std::{fs::File, io::Read};
-use crate::common::types::Address;
 use hex;
-use elliptic_curve;
+use std::error::Error;
 
 
 // keccakState wraps sha3.state. In addition to the usual hash methods, it also supports
 // Read to get a variable amount of data from the hash state. Read is faster than Sum
 // because it doesn't copy the internal state, but also modifies the internal state.
 pub trait KeccakState {
-}
-
-pub fn pubkey_to_address(p : PublicKey) -> Address {
-    todo!();
 }
 
 // LoadECDSA loads a secp256k1 private key from the given file.
@@ -30,9 +25,32 @@ pub fn load_ecdsa(file : &str) -> SecretKey {
 // controls whether the key's length should be enforced at the curve size or
 // it can also accept legacy encodings (0 prefixes).
 pub fn to_ecdsa(d: Vec<u8>) -> Result<SecretKey, Box<dyn std::error::Error>> {
-    // let priv_key : secp256k1::SecretKey;
-    
-    // let secp = Secp256k1::new();
     let priv_key = SecretKey::from_slice(&d).expect("32 bytes, within curve order");
     Ok(priv_key)
 }
+
+// HexToECDSA parses a secp256k1 private key.
+pub fn hex_to_ecdsa(hexkey: String) -> Result<secp256k1::SecretKey, Box<dyn Error>> {
+    let b = hex::decode(hexkey).expect("invalid hex string");
+    to_ecdsa(b)
+}
+
+pub fn generate_key() -> secp256k1::SecretKey {
+    let secp = Secp256k1::new();
+    let (secret_key, _) = secp.generate_keypair(&mut OsRng);
+    
+    secret_key
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_generate_key_ok() {
+        let sec = generate_key();
+        println!("{}", sec.display_secret())
+    }
+}
+
