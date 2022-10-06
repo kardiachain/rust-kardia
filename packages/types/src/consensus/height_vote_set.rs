1ee98f069965d1d853293ba4e7ec9c414a1846f7
phatngluu <yadsendew@protonmail.com>
2022-10-06 17:56:16 +0700

    feat: height vote set and vote set

--- packages/types/src/consensus/height_vote_set.rs
+++ packages/types/src/consensus/height_vote_set.rs
@@ -40,8 +40,68 @@ impl HeightVoteSet {
         todo!()
     }
 
-    pub fn add_vote(&mut self, vote: Vote) -> Result<(), String> {
-        todo!()
+    pub fn add_vote(&mut self, vote: Vote, peer_id: PeerId) -> Result<(), String> {
+        let vote_set = match self.get_mut_vote_set(vote.round, vote.r#type()) {
+            Some(vs) => vs,
+            None => {
+                let rounds = self.peer_catchup_rounds.get_mut(&peer_id);
+                if rounds.is_none() || rounds.is_some_and(|r| r.len() < 2) {
+                    if rounds.is_none() {
+                        self.peer_catchup_rounds.insert(peer_id, vec![vote.round]);
+                    } else {
+                        rounds.unwrap().push(vote.round);
+                    }
+                    // init vote set for round
+                    self.add_round(vote.round);
+                    // return newly created vote set
+                    self.get_mut_vote_set(vote.round, vote.r#type()).unwrap()
+                } else {
+                    return Err("ErrGotVoteFromUnwantedRound".to_owned());
+                }
+            }
+        };
+
+        return vote_set.add_vote(vote);
+    }
+
+    fn get_mut_vote_set(
+        &mut self,
+        round: u32,
+        signed_msg_type: SignedMsgType,
+    ) -> Option<&mut VoteSet> {
+        if let Some(rvs) = self.round_vote_sets.get_mut(&round) {
+            match signed_msg_type {
+                SignedMsgType::Prevote => rvs.prevotes.as_mut(),
+                SignedMsgType::Precommit => rvs.precommits.as_mut(),
+                _ => None,
+            }
+        } else {
+            None
+        }
+    }
+
+    fn add_round(&mut self, round: u32) {
+        let prevotes = VoteSet::new(
+            self.chain_id.clone(),
+            self.height,
+            round,
+            SignedMsgType::Prevote,
+            self.validator_set.clone().unwrap(),
+        );
+        let precommits = VoteSet::new(
+            self.chain_id.clone(),
+            self.height,
+            round,
+            SignedMsgType::Precommit,
+            self.validator_set.clone().unwrap(),
+        );
+        self.round_vote_sets.insert(
+            round,
+            RoundVoteSet {
+                prevotes: Some(prevotes),
+                precommits: Some(precommits),
+            },
+        );
     }
 }
 
--- packages/types/src/vote_set.rs
+++ packages/types/src/vote_set.rs
@@ -1,15 +1,48 @@
 use std::fmt::Debug;
 
-use crate::{bit_array::BitArray, block::BlockId, commit::Commit, vote::Vote};
+use crate::{
+    bit_array::BitArray, block::BlockId, commit::Commit, consensus::state::ChainId,
+    types::SignedMsgType, validator_set::ValidatorSet, vote::Vote,
+};
 
 pub trait VoteSetReader: Debug + Sync + Send + 'static {}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct VoteSet {
+    pub chain_id: ChainId,
+    pub height: u64,
+    pub round: u32,
+    pub validator_set: ValidatorSet,
+    pub votes_bit_array: BitArray,
+    pub votes: Vec<Option<Vote>>,
+    pub sum: u64,
+    pub r#type: SignedMsgType,
     pub maj23: Option<BlockId>,
+    // votesByBlock  map[string]*blockVotes // string(blockHash|blockParts) -> blockVotes
+    // peerMaj23s    map[p2p.ID]BlockID     // Maj23 for each peer
 }
 
 impl VoteSet {
+    pub fn new(
+        chain_id: ChainId,
+        height: u64,
+        round: u32,
+        signed_msg_type: SignedMsgType,
+        validator_set: ValidatorSet,
+    ) -> Self {
+        Self {
+            chain_id: chain_id,
+            height: height,
+            round: round,
+            r#type: signed_msg_type,
+            validator_set: validator_set.clone(),
+            votes_bit_array: BitArray::new_bit_array(validator_set.clone().validators.len()),
+            votes: vec![None; validator_set.validators.clone().len()],
+            sum: 0,
+            maj23: None,
+        }
+    }
+
     pub fn bit_array(&self) -> Option<BitArray> {
         todo!()
     }
