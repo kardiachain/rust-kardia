5a961e2228aea0d65db864e9bb0b9a3f9c685d73
thangn <thangn.1411@gmail.com>
2022-10-11 16:09:44 +0700

    feat: add tx pool, hasher, transaction

--- packages/primitives/core/src/lib.rs
+++ packages/primitives/core/src/lib.rs
@@ -1,9 +1,16 @@
-pub mod  hash;
-pub mod  ecdsa;
-pub mod  crypto;
-pub mod  hashing;
-pub mod  hexdisplay;
-pub mod  uint;
+pub mod crypto;
+pub mod ecdsa;
+pub mod hash;
+pub mod hasher;
+pub mod hashing;
+pub mod hexdisplay;
+pub mod uint;
+
+#[cfg(feature = "full_crypto")]
+pub use hashing::{blake2_128, blake2_256, keccak_256, twox_128, twox_256, twox_64};
 
 #[cfg(feature = "full_crypto")]
 pub use crypto::{ByteArray, DeriveJunction, Pair, Public};
+
+#[cfg(feature = "std")]
+pub use self::hasher::keccak::KeccakHasher;
