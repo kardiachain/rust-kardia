89ccb942d1b0695ed18be13810e1c2552d2b8591
thangn <thangn.1411@gmail.com>
2022-10-11 18:50:30 +0700

    feat: add trie

--- packages/primitives/core/src/lib.rs
+++ packages/primitives/core/src/lib.rs
@@ -1,7 +1,8 @@
 pub mod crypto;
 pub mod ecdsa;
 pub mod hash;
-pub mod hasher;
+#[cfg(feature = "std")]
+mod hasher;
 pub mod hashing;
 pub mod hexdisplay;
 pub mod uint;
@@ -13,4 +14,14 @@ pub use hashing::{blake2_128, blake2_256, keccak_256, twox_128, twox_256, twox_6
 pub use crypto::{ByteArray, DeriveJunction, Pair, Public};
 
 #[cfg(feature = "std")]
+pub use self::hasher::blake2::Blake2Hasher;
+#[cfg(feature = "std")]
 pub use self::hasher::keccak::KeccakHasher;
+
+pub use self::{
+	hash::{convert_hash, H160, H256, H512},
+	uint::{U256, U512},
+};
+
+pub use kp_storage as storage;
+pub use hash_db::Hasher;
